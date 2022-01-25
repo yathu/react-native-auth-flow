949d28a7189bfeae84a406ee221bba7d97ffb267
yathavan <yathavancom@gmail.com>
2022-01-25 09:29:51 +0530

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
